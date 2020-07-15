12a1345029139014e1042d7da5ea8d80af1be8fd
chefkasperson <chefkasperson@gmail.com>
2020-07-15 14:05:24 -0700

    added about route

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express()
 app.get('/', function(request, response) {
     response.send('<h1>Hello World!</h1>')
 })
+
+app.get('/contact', function(req, res) {
+    res.send('contact me at chefkasperson@gmail.com')
+})
+
+app.get('/about', function(req, res) {
+    res.send('my name is Erik and I am a chef turned software engineer')
+})
 app.listen(3000, function() {
     console.log('server listening on port:3000')
 })
