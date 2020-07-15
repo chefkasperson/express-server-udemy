a877d12e4f0db694f6de966a317a44aec3a9c9aa
chefkasperson <chefkasperson@gmail.com>
2020-07-15 13:51:31 -0700

    added server response

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', function(request, response) {
+    response.send('<h1>Hello World!</h1>')
+})
 app.listen(3000, function() {
     console.log('server listening on port:3000')
 })
